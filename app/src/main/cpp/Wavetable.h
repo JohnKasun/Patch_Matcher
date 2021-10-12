9154d3ceeb72c8eb0eb6f9e9aeb6b6e36f86c7ad
John Kasun <johnkasun@live.com>
2021-10-12 19:44:09 -0400

    Add Wavetable outline

--- app/src/main/cpp/Wavetable.h
+++ app/src/main/cpp/Wavetable.h
@@ -4,9 +4,23 @@
 
 #ifndef PATCH_MATCHER_WAVETABLE_H
 #define PATCH_MATCHER_WAVETABLE_H
+#include <functional>
 
 
 class Wavetable {
+private:
+    unsigned size;
+    float *table;
+    std::function<float(int)> patch;
+public:
+
+    Wavetable(int size = 128);
+    ~Wavetable();
+
+    void generate();
+    unsigned get_size() const ;
+    float operator[](int index) const;
+    void setPatch(std::function<float(int)> new_patch);
 
 };
 
