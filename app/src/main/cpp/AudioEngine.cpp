b512e3f193768c7d3b3be530f686e0ee680cc84d
John Kasun <johnkasun@live.com>
2021-10-12 16:14:16 -0400

    Implement AudioEngine class

--- app/src/main/cpp/AudioEngine.cpp
+++ app/src/main/cpp/AudioEngine.cpp
@@ -4,6 +4,49 @@
 
 #include "AudioEngine.h"
 
-oboe::DataCallbackResult AudioEngine::onAudioReady(oboe::AudioStream *audioStream, void *audioData, int32_t numFrames) {
+AudioEngine::AudioEngine(){}
 
+int32_t AudioEngine::startAudio()
+{
+    std::lock_guard<std::mutex> lock(mLock);
+    oboe::AudioStreamBuilder builder;
+    // The builder set methods can be chained for convenience.
+    oboe::Result result = builder.setSharingMode(oboe::SharingMode::Exclusive)
+            ->setPerformanceMode(oboe::PerformanceMode::LowLatency)
+            ->setChannelCount(kChannelCount)
+            ->setSampleRate(kSampleRate)
+            ->setSampleRateConversionQuality(oboe::SampleRateConversionQuality::Medium)
+            ->setFormat(oboe::AudioFormat::Float)
+            ->setDataCallback(this)
+            ->openStream(mStream);
+    if (result != oboe::Result::OK) return (int32_t) result;
+
+    // Typically, start the stream after querying some stream information, as well as some input from the user
+    result = mStream->requestStart();
+    return (int32_t) result;
+}
+
+void AudioEngine::stopAudio()
+{
+    // Stop, close and delete in case not already closed.
+    std::lock_guard<std::mutex> lock(mLock);
+    if (mStream) {
+        mStream->stop();
+        mStream->close();
+        mStream.reset();
+    }
+}
+
+oboe::DataCallbackResult AudioEngine::onAudioReady(oboe::AudioStream *audioStream, void *audioData, int32_t numFrames)
+{
+    float *floatData = (float *) audioData;
+    for (int i = 0; i < numFrames; ++i) {
+        float sampleValue = kAmplitude * sinf(mPhase);
+        for (int j = 0; j < kChannelCount; j++) {
+            floatData[i * kChannelCount + j] = sampleValue;
+        }
+        mPhase += mPhaseIncrement;
+        if (mPhase >= kTwoPi) mPhase -= kTwoPi;
+    }
+    return oboe::DataCallbackResult::Continue;
 }
--- app/src/main/cpp/AudioEngine.h
+++ app/src/main/cpp/AudioEngine.h
@@ -11,10 +11,26 @@ class AudioEngine : public oboe::AudioStreamCallback{
 public:
     AudioEngine();
     virtual ~AudioEngine() = default;
+
     int32_t startAudio();
     void stopAudio();
     oboe::DataCallbackResult onAudioReady(oboe::AudioStream *audioStream, void *audioData, int32_t numFrames) override;
+
 private:
+    std::mutex         mLock;
+    std::shared_ptr<oboe::AudioStream> mStream;
+
+    // Stream params
+    static int constexpr kChannelCount = 2;
+    static int constexpr kSampleRate = 48000;
+    // Wave params, these could be instance variables in order to modify at runtime
+    static float constexpr kAmplitude = 0.5f;
+    static float constexpr kFrequency = 440;
+    static float constexpr kPI = M_PI;
+    static float constexpr kTwoPi = kPI * 2;
+    static double constexpr mPhaseIncrement = kFrequency * kTwoPi / (double) kSampleRate;
+    // Keeps track of where the wave is
+    float mPhase = 0.0;
 };
 
 
