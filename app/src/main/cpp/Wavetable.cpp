23fa813de8ffa14df13e2ec695d7831cc920acea
John Kasun <johnkasun@live.com>
2021-10-12 19:44:23 -0400

    Implement wavetable functions

--- app/src/main/cpp/Wavetable.cpp
+++ app/src/main/cpp/Wavetable.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Wavetable.h"
+
+Wavetable::Wavetable(int size)
+    : size(size), table(nullptr),
+      patch( [](int index){ return sinf(index);} )
+{
+    table = new float[size];
+}
+
+Wavetable::~Wavetable()
+{
+    delete[] table;
+}
+
+void Wavetable::generate()
+{
+    int index =
+    table[index] = patch(index);
+}
+
+unsigned Wavetable::get_size() const
+{
+    return size;
+}
+
+float Wavetable::operator[](int index) const
+{
+
+};
+
+void Wavetable::setPatch(std::function<float(int)> new_patch)
+{
+    patch = new_patch;
+}
