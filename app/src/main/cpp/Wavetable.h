4999373f91d830125e7358808e6620e57fed0c69
John Kasun <johnkasun@live.com>
2021-10-12 19:18:50 -0400

    add Wavetable class files

---
+++ app/src/main/cpp/Wavetable.h
@@ -0,0 +1,14 @@
+//
+// Created by JohnK on 10/12/2021.
+//
+
+#ifndef PATCH_MATCHER_WAVETABLE_H
+#define PATCH_MATCHER_WAVETABLE_H
+
+
+class Wavetable {
+
+};
+
+
+#endif //PATCH_MATCHER_WAVETABLE_H
