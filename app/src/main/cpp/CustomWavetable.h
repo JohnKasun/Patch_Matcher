3c90a2343ddc81f46666f8fa3bf29cd8d18e64f5
John Kasun <johnkasun@live.com>
2021-10-15 12:07:00 -0400

    Add CustomWavetable class files

---
+++ app/src/main/cpp/CustomWavetable.h
@@ -0,0 +1,14 @@
+//
+// Created by JohnK on 10/15/2021.
+//
+
+#ifndef PATCH_MATCHER_CUSTOMWAVETABLE_H
+#define PATCH_MATCHER_CUSTOMWAVETABLE_H
+
+
+class CustomWavetable {
+
+};
+
+
+#endif //PATCH_MATCHER_CUSTOMWAVETABLE_H
