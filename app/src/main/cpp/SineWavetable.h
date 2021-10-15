4c09dfcfe1146320d9b08876d63b43e20e280eb9
John Kasun <johnkasun@live.com>
2021-10-15 12:00:14 -0400

    Add SineWavetable class files

---
+++ app/src/main/cpp/SineWavetable.h
@@ -0,0 +1,14 @@
+//
+// Created by JohnK on 10/15/2021.
+//
+
+#ifndef PATCH_MATCHER_SINEWAVETABLE_H
+#define PATCH_MATCHER_SINEWAVETABLE_H
+
+
+class SineWavetable {
+
+};
+
+
+#endif //PATCH_MATCHER_SINEWAVETABLE_H
