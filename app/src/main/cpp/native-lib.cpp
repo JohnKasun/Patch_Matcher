66f7c4b71284da14c3c3eb49a910f5997d26406b
John Kasun <johnkasun@live.com>
2021-10-15 12:29:06 -0400

    Create AudioEngine object in natve-lib.cpp for verification

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include "AudioEngine.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_patch_1matcher_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    AudioEngine engine;
     return env->NewStringUTF(hello.c_str());
 }
