ea67be5dd02090b8bb40089acf621a6e5bb66553
John Kasun <johnkasun@live.com>
2021-10-15 12:02:56 -0400

    Implement methods

--- app/src/main/cpp/SineWavetable.cpp
+++ app/src/main/cpp/SineWavetable.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "SineWavetable.h"
+SineWavetable::SineWavetable() : Wavetable()
+{}
+
+void SineWavetable::generate()
+{
+    auto angleDelta = kTwoPi / (double)(size - 1);
+    auto currentAngle = 0.0;
+
+    for (unsigned i = 0; i < size; ++i)
+    {
+        table[i] = sinf(currentAngle);
+        currentAngle += angleDelta;
+    }
+}
