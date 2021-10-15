4e55e4723e30596cf99461e69f24d6f313cea5d0
John Kasun <johnkasun@live.com>
2021-10-15 12:09:17 -0400

    Implement methods

--- app/src/main/cpp/CustomWavetable.cpp
+++ app/src/main/cpp/CustomWavetable.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "CustomWavetable.h"
+CustomWavetable::CustomWavetable(std::function<float(double)> patch) : Wavetable(), patch{patch}
+{
+}
+
+CustomWavetable::~CustomWavetable()
+{
+}
+
+bool CustomWavetable::setPatch(std::function<float(double)> new_patch)
+{
+    patch = new_patch;
+    return true;
+}
+
+void CustomWavetable::generate()
+{
+    auto angleDelta = kTwoPi / (double)(size - 1);
+    auto currentAngle = 0.0;
+
+    for (unsigned i = 0; i < size; ++i)
+    {
+        table[i] = patch(currentAngle);
+        currentAngle += angleDelta;
+    }
+}
