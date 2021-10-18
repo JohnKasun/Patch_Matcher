4d0004a3f839c5af379b7372d752b1c3980dcb40
John Kasun <johnkasun@live.com>
2021-10-17 23:51:29 -0400

    Add Tester class files

---
+++ app/src/main/cpp/Tester.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JohnK on 10/17/2021.
+//
+
+#include "Tester.h"
---
+++ app/src/main/cpp/Tester.h
@@ -0,0 +1,14 @@
+//
+// Created by JohnK on 10/17/2021.
+//
+
+#ifndef PATCH_MATCHER_TESTER_H
+#define PATCH_MATCHER_TESTER_H
+
+
+class Tester {
+
+};
+
+
+#endif //PATCH_MATCHER_TESTER_H
