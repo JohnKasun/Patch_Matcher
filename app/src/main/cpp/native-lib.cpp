ec5197028e301765f095ac741eaaf33c47d97c6d
John Kasun <johnkasun@live.com>
2021-10-22 10:13:06 -0400

    Add play and stop button to GUI

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,11 +2,22 @@
 #include <string>
 #include "AudioEngine.h"
 
+AudioEngine engine;
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_patch_1matcher_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    AudioEngine engine;
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_patch_1matcher_MainActivity_onPlayButtonPress(JNIEnv *env, jobject thiz) {
+    engine.startAudio();
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_patch_1matcher_MainActivity_onStopButtonPress(JNIEnv *env, jobject thiz) {
+    engine.stopAudio();
 }
