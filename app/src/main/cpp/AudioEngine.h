17753782fc929e1deb1af179fee78a848c8ae38e
John Kasun <johnkasun@live.com>
2021-10-22 10:13:27 -0400

    Integrate Wavetable classes with AudioEngine

--- app/src/main/cpp/AudioEngine.h
+++ app/src/main/cpp/AudioEngine.h
@@ -32,16 +32,16 @@ private:
     static int constexpr kSampleRate = 48000;
     // Wave params, these could be instance variables in order to modify at runtime
     static float constexpr kAmplitude = 0.5f;
-    static float constexpr kFrequency = 440;
     static float constexpr kPI = M_PI;
     static float constexpr kTwoPi = kPI * 2;
-    static double constexpr mPhaseIncrement = kFrequency * kTwoPi / (double) kSampleRate;
-    // Keeps track of where the wave is
-    float mPhase = 0.0;
 
     //Wavetables
     SineWavetable sine;
     SquareWavetable square;
+    CustomWavetable custom;
+
+    WavetableOscillator osc;
+    WavetableOscillator osc2;
 
     //load Wavetables on startup
     void loadWavetables();
