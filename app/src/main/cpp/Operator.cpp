2e90cfdf8ba2595810479a439699c02304c0eb48
John Kasun <johnkasun@live.com>
2021-11-04 15:10:30 -0400

    Add Operator class

---
+++ app/src/main/cpp/Operator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JohnK on 11/4/2021.
+//
+
+#include "Operator.h"
---
+++ app/src/main/cpp/Operator.h
@@ -0,0 +1,14 @@
+//
+// Created by JohnK on 11/4/2021.
+//
+
+#ifndef PATCH_MATCHER_OPERATOR_H
+#define PATCH_MATCHER_OPERATOR_H
+
+
+class Operator {
+
+};
+
+
+#endif //PATCH_MATCHER_OPERATOR_H
