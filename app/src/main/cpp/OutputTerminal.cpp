6857b59bfed89615bb202f0fcbe0e7d537d79c8f
John Kasun <johnkasun@live.com>
2021-11-11 20:19:24 -0500

    Add OutputTerminal class files

---
+++ app/src/main/cpp/OutputTerminal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JohnK on 11/11/2021.
+//
+
+#include "OutputTerminal.h"
---
+++ app/src/main/cpp/OutputTerminal.h
@@ -0,0 +1,14 @@
+//
+// Created by JohnK on 11/11/2021.
+//
+
+#ifndef PATCH_MATCHER_OUTPUTTERMINAL_H
+#define PATCH_MATCHER_OUTPUTTERMINAL_H
+
+
+class OutputTerminal {
+
+};
+
+
+#endif //PATCH_MATCHER_OUTPUTTERMINAL_H
