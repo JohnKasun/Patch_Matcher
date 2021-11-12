2cce8195fbec1de4c1d71cd4b98a94a9f68aab20
John Kasun <johnkasun@live.com>
2021-11-11 20:31:35 -0500

    Add basic functionality for OutputTerminal

--- app/src/main/cpp/OutputTerminal.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by JohnK on 11/11/2021.
-//
-
-#include "OutputTerminal.h"
--- app/src/main/cpp/OutputTerminal.h
+++ app/src/main/cpp/OutputTerminal.h
@@ -4,10 +4,24 @@
 
 #ifndef PATCH_MATCHER_OUTPUTTERMINAL_H
 #define PATCH_MATCHER_OUTPUTTERMINAL_H
+#include "Operator.h"
+#include <list>
 
 
 class OutputTerminal {
-
+private:
+    std::list<Operator*> outputOperators;
+public:
+    OutputTerminal(){};
+    ~OutputTerminal(){};
+    void addOperator(Operator* operatorToAdd) {outputOperators.push_back(operatorToAdd);};
+    void removeOperator(Operator* operatorToRemove) {outputOperators.remove(operatorToRemove);};
+    inline float getNextSample() const noexcept {
+        float currentSample {0};
+        for (const auto &outputOperator : outputOperators)
+            currentSample += outputOperator->getNextSample();
+        return currentSample;
+    }
 };
 
 
